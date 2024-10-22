2a0c8960ad6a489f90cbd46d8f1207fc8aeb42c5
khosta77 <puwerfulpants@gmail.com>
2024-10-22 16:28:24 +0300

    Тест

---
+++ test_make.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python scripts/PostProcess.py test/test1.inp test/test1.out initial1.png deformed1.png
+python scripts/PostProcess.py test/test2.inp test/test2.out initial2.png deformed2.png
+python scripts/PostProcess.py test/test3.inp test/test3.out initial3.png deformed3.png
+python scripts/PostProcess.py test/test4.inp test/test4.out initial4.png deformed4.png

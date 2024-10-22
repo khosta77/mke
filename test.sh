90d7a8c09b42c8a93bf529e60262f08bec4efa88
khosta77 <puwerfulpants@gmail.com>
2024-10-22 16:38:08 +0300

    Изменил название тестового исполнителя

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+./main.out test/test1.inp test/test1.out
 python scripts/PostProcess.py test/test1.inp test/test1.out initial1.png deformed1.png
+
+./main.out test/test2.inp test/test2.out
 python scripts/PostProcess.py test/test2.inp test/test2.out initial2.png deformed2.png
+
+./main.out test/test4.inp test/test4.out
 python scripts/PostProcess.py test/test3.inp test/test3.out initial3.png deformed3.png
+
+./main.out test/test4.inp test/test4.out
 python scripts/PostProcess.py test/test4.inp test/test4.out initial4.png deformed4.png
